9258f1bbff6c8f30c3a9ff97bdb32bf2c577f7fb
alminaildirar <55828986+alminaildirar@users.noreply.github.com>
2021-12-29 17:07:55 +0300

    Update index.js

--- Homework6/index.js
+++ Homework6/index.js
@@ -1,1 +1,23 @@
 
+const koa = require('koa');
+const route = require('koa-route')
+const app = new koa();
+const port = 3000;
+
+app.use(route.get('/', index));
+app.use(route.get('/about', about))
+app.use(route.get('/contact', contact))
+
+function *index(){
+ this.body= '<h1>Welcome to the index page.</h1>';
+}
+
+function *about(){
+ this.body= '<h1>Welcome to the about page.</h1>';
+}
+
+function *contact(){
+ this.body= '<h1>Welcome to the contact page.</1>';
+}
+
+app.listen(port)
